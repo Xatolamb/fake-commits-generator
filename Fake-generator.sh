7473c3e3cc5d0ea5f1bfc7a89e3370f2f7f07e98
Xatolamb <98926715+Xatolamb@users.noreply.github.com>
2022-02-04 06:04:48 +0530

    Update Fake-generator.sh

--- Fake-generator.sh
+++ Fake-generator.sh
@@ -5,7 +5,7 @@ a=$(shuf -i 1-$size -n $total_commit | sort -r -n)
 git checkout -b gh-pages
 for i in ${a}
 do 
-git commit --allow-empty -m "Fake commit"
+git commit --allow-empty -m "Success"
 git commit --amend --allow-empty --no-edit --date "$(date -d "-$i minutes")"
 done 
 git push origin gh-pages
